3c08ab2292810a9e1fe7ef22c4ce304f565f8850
Gayathri Swaminathan <53771795+gaaithri@users.noreply.github.com>
2024-12-20 22:20:38 +0530

    Add files via upload

---
+++ PythonIntro.py
@@ -0,0 +1,196 @@
+# %%
+# variables 
+
+# %%
+apple = 10 
+banana = 20 
+grapes = 30 
+
+# %%
+apple + banana
+
+# %%
+grapes = 30 
+
+# %% [markdown]
+# 2 kgs of apple + 3 kgs grapes + 1 kg banana 
+
+# %%
+"The total cost of fruit = ? " 
+
+# %%
+input("what is your name?")
+
+# %%
+res = input("what is your name ")
+
+# %%
+print(res)
+
+# %%
+favcolur =  input (" what is your favourite colour")
+
+# %%
+food = input ( " Tell me your favourite food ")
+
+# %% [markdown]
+# My Name is Nimish , My fav colour is <colour> and I love <foodname>
+
+# %%
+print("My name is " + res +"\n"+ " " + "My Fav Colour " + favcolur  + " I love" + food)
+
+# %%
+
+Generate Club Names 
+
+you will ask sports 
+name of best friend 
+name the pet 
+
+Tennis 
+Sanjay 
+Sathu 
+
+You can name your  Sports Club  = SanjaySathu Tennis Club 
+SathuSanjay TEnnis Club 
+SanSat Tennis Club .
+
+
+# %% [markdown]
+# marks = [ 45, 44, 36 ,35, 60]
+
+# %%
+marks = [ 45, 44, 36 ,35, 60]
+
+# %% [markdown]
+# elements of list -> Marks 
+# len( marks )
+
+# %%
+p = len(marks )
+
+# %%
+print(p)
+
+# %%
+marks [0]
+
+# %%
+marks [1]
+
+# %% [markdown]
+# number of students in a grade of school across 6 section .
+
+# %% [markdown]
+# Make a list with the count of students in each grade . 
+
+# %% [markdown]
+# Grade A   = 50 , Grade b = 35 , Grade C = 40 , GRade D = 56 , Grade E = 35 , Grade F = 46
+
+# %% [markdown]
+# Print the Numbers of GRade B , grade E . 
+
+# %%
+
+
+# %%
+
+
+# %%
+# Creating a List 
+
+# %%
+grade = [50, 35, 40, 56, 35, 46]
+
+# %% [markdown]
+# # list / array 
+
+# %% [markdown]
+# ### Set of values enclosed between Sqaure brackets 
+
+# %% [markdown]
+# accessing the elements of the list 
+
+# %% [markdown]
+# Print only the number of students in Grade B
+
+# %% [markdown]
+# 
+
+# %% [markdown]
+#  ### The Number of Students in Grade B Are 35 
+
+# %% [markdown]
+# The total Number of Students in Grade A, B and C
+# Output Format :  
+#  The Total Number of Students in Grade A, B ,C are : 
+
+# %%
+print ( "The Number of Students in Grade B are" , grade[1])
+
+# %%
+count = len(happyGrade)
+print("Number of sections in Happy Grade", count)
+
+# %%
+happyGrade_before_newAdmission = [45,65,46,53,60,78,51,32,12,18,30,46,45,33,89]
+
+# %%
+
+
+# %% [markdown]
+#  ### FOR Loops
+
+# %%
+index = -1
+happyGrade_after_newAdmission = []
+for i in happyGrade_before_newAdmission: 
+    index = index + 1
+    happyGrade_after_newAdmission.append(i+3)
+
+print("before Admission",happyGrade_before_newAdmission)
+print("After Admission ", happyGrade_after_newAdmission)
+
+# %% [markdown]
+# 
+
+# %%
+count = 0 
+index = -1
+for i in happyGrade:
+    index = index + 1
+    count = count + i
+    print(count, " Now adding count for ", index)
+print(count)
+
+# %%
+beGood =  [ 87, 90, 23, 45, 78 , 109, 23, 45, 78, 90, 96, 97, 100, 78, 150, 98,100,125, 150, 130,150]
+
+# %%
+beGood.append( 45)
+
+# %%
+print(beGood)
+count = len(beGood)
+print(count)
+
+# %%
+print(" The total marks " , )
+
+# %%
+len(beGood)
+
+# %% [markdown]
+# The total Marks Scored By 21 students of beGood school is ___?___
+
+# %% [markdown]
+# 1. Add one more Student mark  in beGood School of 147
+# 2. Add grace marks of 5 to all students 
+
+# %% [markdown]
+# 
+
+# %% [markdown]
+# 
+
+
